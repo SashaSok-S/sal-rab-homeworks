b291584d4d151418226d71a6348f53203e661133
Alexandra Sokolovskaya <Sokolovskaya4321@yandex.ru>
2021-06-20 12:14:45 +0300

    final?

--- task_final/solution.js
+++ task_final/solution.js
@@ -40,12 +40,12 @@ function sendRequest(name, phone, address, goods, sum) {
 
     for (let i = 0; i <= countOfGoods; i += 1) {
         data.goods.push(goods[i].title);
-    }
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
+}
+        data.order.address = address;
+        data.order.sum = sum;
+    
+        data.client = name + " " + phone;
 
     let jsonData = JSON.stringify(data);
 
